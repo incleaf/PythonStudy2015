ea8b8998d79670476a75db6c30cc9fd85fed5ece
Hyeon-Su Lee <incleaf@gmail.com>
2015-11-03 20:21:18 +0900

    weeeeek2

---
+++ week02/hyeonsu/Number.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+integer = 1234 # 정수
+floatingPoint = 4.56 # 실수
+octal = 0o155 # 8진수
+hexademical = 0xABC # 16진수
+complexNumber = 1 + 2j # 복소수
+print(integer, floatingPoint, octal, hexademical, complexNumber)
+
+print(3 * 4) # 3x4
+print(3 ** 4) # 3^4
+print(7 % 3) # 나머지
+print(7 / 3) # 나누기
+print(7 / (3*1.0) ) # 나누기 (강제 형 변환)
+print(7 // (3*1.0) ) # 소숫점 버리기 (형변환 때문인지 잘 안됨)
